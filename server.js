a824cb5590091e412e8ca4cf68dbe64457d2e2ab
Joe <roxbury.joseph@gmail.com>
2016-11-29 21:05:04 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var http = require("http"),
+	express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+	response.end("Hello World!");
+});
+
+const server = new http.Server(app);
+
+const port = 3000;
+
+server.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
+
