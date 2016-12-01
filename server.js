52f7e40c19e3d62ee959b8ecdff177bb54eda947
Joe <roxbury.joseph@gmail.com>
2016-11-30 22:33:21 -0500

    chattin

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 "use strict";
 
 var http = require("http"),
-	express = require("express");
+	express = require("express"),
+	socketIo = require("socket.io");
 
 const app = express();
 app.set("view engine", "jade");
 
 app.use(express.static("./public"));
 
-app.use( (request,response,next) => {
-	console.log("IN");
-	next();
-	console.log("OUT");
-});
-
 app.get("/", (request,response) => {
-	response.render("index", {title: "TITLE!"});
+	response.render("index", {title: "Chat app!"});
 });
 
 const server = new http.Server(app);
+const io = socketIo(server);
+
+io.on("connection", socket => {
+	console.log("Client connected.");
+	socket.on("chat:add", data => {
+		console.log(data);
+		io.emit("chat:added", data);
+	})
+});
 
 const port = 3000;
-
 server.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
